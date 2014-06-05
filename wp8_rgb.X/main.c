920ca593290b00633b6c5c005b695343c9fd8712
Diego <santinelli.diego@gmail.com>
2014-06-05 23:08:39 +0200

    first commit

---
+++ wp8_rgb.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: diego
+ *
+ * Created on 5 giugno 2014, 22.46
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
